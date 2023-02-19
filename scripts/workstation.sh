fa3c3f25e66d326080cba10b0b07ebf92b5c1d59
opnmind <opnmind@mailbox.org>
2023-02-19 16:48:09 +0100

    Update workstation.sh

--- scripts/workstation.sh
+++ scripts/workstation.sh
@@ -28,8 +28,6 @@ mkdir -p /home/vagrant/.kube
 sudo cp -i $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
 sudo chmod 0600 /home/vagrant/.kube/config
-NODENAME=$(hostname -s)
-kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
 EOF
 
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
