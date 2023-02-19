67ad56a6200d880b7d432db8cefb4e3c12b78126
opnmind <opnmind@mailbox.org>
2023-02-19 16:59:21 +0100

    Update workstation.sh

--- scripts/workstation.sh
+++ scripts/workstation.sh
@@ -43,12 +43,12 @@ helm repo update
 
 # Installing dashboard service
 mkdir -p /dashboard
-cat <<EOF >>./dashboard/dashboard.sh
+cat <<EOF >./dashboard/dashboard.sh
 #!/bin/bash
 showtoken=1
 cmd="kubectl proxy"
 count=$(pgrep -cf "\$cmd")
-DASHBOARD_VERSION=$(curl --silent https://api.github.com/repos/kubernetes/dashboard/releases/latest | jq -r .tag_name)
+DASHBOARD_VERSION=\$(curl --silent https://api.github.com/repos/kubernetes/dashboard/releases/latest | jq -r .tag_name)
 DASHBOARD_YAML="https://raw.githubusercontent.com/kubernetes/dashboard/\$DASHBOARD_VERSION/aio/deploy/recommended.yaml"
 msg_started="-e Kubernetes Dashboard e[92mstartede[0m"
 msg_stopped="Kubernetes Dashboard stopped"
@@ -70,7 +70,7 @@ start)
 stop)
    showtoken=0
    if [ \$count -gt 0 ]; then
-      kill -9 $(pgrep -f "\$cmd")
+      kill -9 \$(pgrep -f "\$cmd")
    fi
    kubectl delete -f \$DASHBOARD_YAML >/dev/null 2>&1
    kubectl delete -f ~/dashboard/dashboard-admin.yaml >/dev/null 2>&1
@@ -79,12 +79,12 @@ stop)
    ;;
 
 status)
-   found=$(kubectl get serviceaccount admin-user -n kubernetes-dashboard 2>/dev/null)
+   found=\$(kubectl get serviceaccount admin-user -n kubernetes-dashboard 2>/dev/null)
    if [[ \$count = 0 ]] || [[ \$found = "" ]]; then
       showtoken=0
       echo \$msg_stopped
    else
-      found=$(kubectl get clusterrolebinding admin-user -n kubernetes-dashboard 2>/dev/null)
+      found=\$(kubectl get clusterrolebinding admin-user -n kubernetes-dashboard 2>/dev/null)
       if [[ \$found = "" ]]; then
          nopermission=" but user has no permissions."
          echo "\${msg_started}\${nopermission}"
@@ -100,11 +100,11 @@ esac
 if [ \$showtoken -gt 0 ]; then
    # Show token
    echo "Admin token:"
-   kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
+   kubectl get secret -n kubernetes-dashboard \$(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
    echo
 
    echo "User read-only token:"
-   kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount read-only-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
+   kubectl get secret -n kubernetes-dashboard \$(kubectl get serviceaccount read-only-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
    echo
 fi
 EOF
